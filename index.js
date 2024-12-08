196a1fa072ecdc304183a03292c5732aae593f36
Syed Ammar Ali <ammar.ali.skipq@gmail.com>
2024-12-08 22:49:21 +0500

    renamed file

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+const vehicleRoutes = require('./routes/vehicleRoutes')
+const dbConnect = require('./config/db') / dbConnect()
+
+const app = express()
+
+// Request logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+
+app.use(express.json())
+
+app.use(
+  cors({
+    origin: 'https://location-frontend-three.vercel.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+)
+
+// Routes
+app.use('/api/vehicles', vehicleRoutes)
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).json({ message: 'Server Error', error: err.message })
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
